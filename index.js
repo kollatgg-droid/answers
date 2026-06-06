e8421703d5b6ec5cce635b643abac196cf84de09
kollatgg-droid <kollatgg@gmail.com>
2026-06-06 23:48:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname === "/webhook") {
+    if (url.pathname !== "/webhook") {
+      return new Response("Telegram bot is running");
+    }
+
+    try {
       const update = await request.json();
 
       const message = update.message?.text;
       const chatId = update.message?.chat?.id;
       const userId = update.message?.from?.id;
 
-      const TEACHER_ID = 8510843986; // <-- поменяй на ID училки
+      const TEACHER_ID = 8510843986;
 
       // доступ только учителю
       if (userId !== TEACHER_ID) {
-        return new Response("no access");
+        return new Response("ok");
       }
 
       let answer = "Нет такого задания ❌";
@@ -22,9 +26,11 @@ export default {
       if (message === "2") answer = "Ответ: x = 12";
       if (message === "3") answer = "Ответ: x = -3";
 
-      await fetch(`https://api.telegram.org/bot${env.BOT_TOKEN, {
+      await fetch(`https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`, {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json"
+        },
         body: JSON.stringify({
           chat_id: chatId,
           text: answer
@@ -32,8 +38,8 @@ export default {
       });
 
       return new Response("ok");
+    } catch (e) {
+      return new Response("error");
     }
-
-    return new Response("Telegram bot is running");
   }
 };
