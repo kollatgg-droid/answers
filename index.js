f25dbd5668687b9060edcc4719717496c3a0fce9
kollatgg-droid <kollatgg@gmail.com>
2026-06-07 11:16:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
 export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
+async fetch(request, env) {
+const url = new URL(request.url);
 
-    if (url.pathname !== "/webhook") {
-      return new Response("Telegram bot is running");
-    }
+if (url.pathname !== "/webhook") {  
+  return new Response("Telegram bot is running");  
+}  
 
-    try {
-      const update = await request.json();
+try {  
+  const update = await request.json();  
 
-      const message = update.message?.text;
-      const chatId = update.message?.chat?.id;
-      const userId = update.message?.from?.id;
+  const message = update.message?.text;  
+  const chatId = update.message?.chat?.id;  
+  const userId = update.message?.from?.id;  
 
-      const TEACHER_ID = 8510843986;
+  const TEACHER_ID = 8510843986;  
 
-      // доступ только учителю
-      if (userId !== TEACHER_ID) {
-        return new Response("ok");
-      }
+  // доступ только учителю  
+ // временно отключаем проверку  
 
-      let answer = "Нет такого задания ❌";
+  let answer = "Нет такого задания ❌";  
 
-      if (message === "1") answer = "Ответ: x = 5";
-      if (message === "2") answer = "Ответ: x = 12";
-      if (message === "3") answer = "Ответ: x = -3";
+  if (message === "1") answer = "Ответ: x = 5";  
+  if (message === "2") answer = "Ответ: x = 12";  
+  if (message === "3") answer = "Ответ: x = -3";  
 
-      await fetch(`https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          chat_id: chatId,
-          text: answer
-        })
-      });
+  await fetch(`https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`, {  
+    method: "POST",  
+    headers: {  
+      "Content-Type": "application/json"  
+    },  
+    body: JSON.stringify({  
+      chat_id: chatId,  
+      text: answer  
+    })  
+  });  
 
-      return new Response("ok");
-    } catch (e) {
-      return new Response("error");
-    }
-  }
+  return new Response("ok");  
+} catch (e) {  
+  return new Response("error");  
+}
+
+}
 };
